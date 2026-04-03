367da6db4c3f37cd1cd19dc7fbe0dab969b93a0d
meet <ms.opash@gmail.com>
2026-04-03 13:54:47 +0530

    first commit

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,10 @@
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
 
 cloudinary.config({
-    cloud_name: 'dcygfzrto',
-    api_key: '149999843269455',
-    api_secret: 'PyV7RdPvDwydbceX_oBc2Jw5jvc'
+    cloud_name: process.env.CLOUD_APP,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
 module.exports = cloudinary;
